1d237467278cd10b73d90d4ad82b0abb3985cad5
Raviraj Bhosale <ravirajbhosale619@gmail.com>
2021-07-19 23:59:42 +0530

    Quiz-App

---
+++ end.js
@@ -0,0 +1,35 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener("keyup", () => {
+    // console.log(username.value);
+    saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = e => {
+    console.log("Clicked On Save Button!!");
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+
+    highScores.sort( (a, b) => {
+        return b.score - a.score;
+    });
+
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign("/");
+}
